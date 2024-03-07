f1cf1aadce0f2c8c4ecd0e77ba3e1f5579955a22
rojojun <zoloman316@gmail.com>
2024-03-07 19:36:07 +0900

    AnnotationProcessor 적용

---
+++ src/main/java/com/rojojun/ddd/common/exception/DomainServiceAnnotationProcessor.java
@@ -0,0 +1,30 @@
+package com.rojojun.ddd.common.exception;
+
+import com.rojojun.ddd.common.annotation.DomainService;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+@SupportedAnnotationTypes("com.rojojun.ddd.common.annotation.DomainService")
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
+public class DomainServiceAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (Element element : roundEnv.getElementsAnnotatedWith(DomainService.class)) {
+            String packageName = processingEnv.getElementUtils().getPackageOf(element).getQualifiedName().toString();
+            if (!packageName.contains(".domain.")) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                        "@DomainService 어노테이션은 'domain' 패키지에만 허용됩니다.", element);
+            }
+        }
+        return true;
+    }
+}

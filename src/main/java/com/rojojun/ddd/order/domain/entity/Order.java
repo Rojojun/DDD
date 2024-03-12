0195b7beb08aae7b2ddd3b57523287aaf80b4bbc
rojojun <zoloman316@gmail.com>
2024-03-13 00:00:45 +0900

    Annotation Processor 테스트 #3

--- src/main/java/com/rojojun/ddd/order/domain/entity/Order.java
+++ src/main/java/com/rojojun/ddd/order/domain/entity/Order.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;

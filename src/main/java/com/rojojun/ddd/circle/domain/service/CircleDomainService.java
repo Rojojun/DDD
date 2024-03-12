52ee9f51049ddb3b1b429404a14c01be675e57cc
rojojun <zoloman316@gmail.com>
2024-03-12 23:33:01 +0900

    Annotation 적용

--- src/main/java/com/rojojun/ddd/circle/domain/service/CircleDomainService.java
+++ src/main/java/com/rojojun/ddd/circle/domain/service/CircleDomainService.java
@@ -1,10 +1,11 @@
 package com.rojojun.ddd.circle.domain.service;
 
+import com.rojojun.ddd.common.annotation.DomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
-@Service
+@DomainService
 public class CircleDomainService {
     private final CircleDomainService circleDomainService;
 }

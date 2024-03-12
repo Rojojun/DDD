9869b2af347d9b15a3e8242e0b2beec346991c91
rojojun <zoloman316@gmail.com>
2024-03-13 00:02:41 +0900

    Annotation Processor 테스트 #4

--- src/main/java/com/rojojun/ddd/processor/DomainServiceAnnotationProcessor.java
+++ src/main/java/com/rojojun/ddd/processor/DomainServiceAnnotationProcessor.java
@@ -6,6 +6,8 @@ import com.rojojun.ddd.common.annotation.DomainService;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.util.Set;
@@ -17,11 +19,16 @@ public class DomainServiceAnnotationProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        for (Element element : roundEnv.getElementsAnnotatedWith(DomainService.class)) {
-            String packageName = processingEnv.getElementUtils().getPackageOf(element).getQualifiedName().toString();
-            if (!packageName.contains(".domain.")) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                        "@DomainService 어노테이션은 'domain' 패키지에만 허용됩니다.", element);
+        for (TypeElement annotation : annotations) {
+            for (Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
+                if (element.getKind() == ElementKind.CLASS) {
+                    PackageElement packageElement = (PackageElement) element.getEnclosingElement();
+                    String packageName = packageElement.getQualifiedName().toString();
+                    if (!packageName.contains("Service")) {
+                        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                                "Classes annotated with @SpecialService must be in a package containing 'Service'.", element);
+                    }
+                }
             }
         }
         return true;
